a1a513f3125b8d29d102a8fc64d00b5a8a63cea5
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-25 09:59:21 -0800

    Add hatch mode, sleeping, age & weight

--- Tamagotchi/Models/Pet.cs
+++ Tamagotchi/Models/Pet.cs
@@ -12,10 +12,20 @@ namespace Tamagotchis.Models
     public int Fullness { get; set; }
     public int Energy { get; set; }
     public int Attention { get; set; }
+    public int Age { get; set; }
+    public int Weight { get; set; } = 1;
     public bool Alive { get; set; } = true;
     public int Id { get; }
-    
+    private static int nextId = 1;
+
     private Timer timer;
+    private Timer ageTimer;
+    private Timer sleepTimer;
+    private Timer playTimer;
+    public bool IsSleeping { get; set; } = false;
+
+    private Timer incubationTimer;
+    public bool IsHatched { get; set; } = false;
 
     private static List<Pet> _instances = new List<Pet> { };
 
@@ -23,12 +33,10 @@ namespace Tamagotchis.Models
     {
       return _instances;
     }
-
     public static void ClearAll()
     {
       _instances.Clear();
     }
-
     public static void Delete(int petId)
     {
       var petToDelete = _instances.FirstOrDefault(p => p.Id == petId);
@@ -37,26 +45,42 @@ namespace Tamagotchis.Models
         _instances.Remove(petToDelete);
       }
     }
+    public static Pet Find(int searchId)
+    {
+      return _instances.FirstOrDefault(Pet => Pet.Id == searchId);
+    }
+
     public Pet()
     {
       Name = "Jim default";
       Fullness = 100;
-      Energy = 100;
+      Energy = 0;
       Attention = 100;
 
       timer = new Timer();
       timer.Interval = 10000;
       timer.Elapsed += TimerElapsed;
       timer.Start();
-      
+
       _instances.Add(this);
       Id = _instances.Count;
+
+      incubationTimer = new Timer();
+      incubationTimer.Interval = 10000;
+      incubationTimer.Elapsed += HatchTimerElapsed;
+      incubationTimer.Start();
+
+      ageTimer = new Timer();
+      ageTimer.Interval = 86400000; // 24 hours in milliseconds
+      ageTimer.Elapsed += AgeTimerElapsed;
+      ageTimer.Start();
     }
+
     public Pet(string name)
     {
       Name = name;
       Fullness = 100;
-      Energy = 100;
+      Energy = 0;
       Attention = 100;
 
       timer = new Timer();
@@ -65,12 +89,69 @@ namespace Tamagotchis.Models
       timer.Start();
 
       _instances.Add(this);
-      Id = _instances.Count;
+      Id = nextId;
+      nextId++;
+
+      incubationTimer = new Timer();
+      incubationTimer.Interval = 10000;
+      incubationTimer.Elapsed += HatchTimerElapsed;
+      incubationTimer.Start();
+
+      ageTimer = new Timer();
+      ageTimer.Interval = 86400000; // 24 hours in milliseconds
+      ageTimer.Elapsed += AgeTimerElapsed;
+      ageTimer.Start();
+    }
+
+    private void HatchTimerElapsed(object sender, ElapsedEventArgs e)
+    {
+      IsHatched = true;
+      Energy = 100;
+      ((Timer)sender).Stop();
+    }
+
+    private void AgeTimerElapsed(object sender, ElapsedEventArgs e)
+    {
+      Age += 1;
+    }
+
+    private void SleepTimerElapsed(object sender, ElapsedEventArgs e)
+    {
+      IsSleeping = false;
+      Energy = 100;
+      ((Timer)sender).Stop();
     }
 
     private void TimerElapsed(object sender, ElapsedEventArgs e)
     {
       DecreaseAttributes();
+    }
+
+    private void PlayTimerElapsed(object sender, ElapsedEventArgs e)
+    {
+      CheckIfDead();
+      if (Fullness < 5)
+      {
+        Fullness = 0;
+      }
+      if (Energy < 5)
+      {
+        Energy = 0;
+      }
+      else
+      {
+        Fullness -= 5;
+        Energy -= 5;
+        if (Attention < 50)
+        {
+          Attention += 50;
+        }
+        else 
+        {
+          Attention = 100;
+        }
+      }
+      ((Timer)sender).Stop();
     }
 
     private void DecreaseAttributes()
@@ -79,9 +160,10 @@ namespace Tamagotchis.Models
       Fullness -= 2;
       Attention -= 2;
 
-      if (Energy < 0)
+      if (Energy <= 0)
       {
         Energy = 0;
+        Sleep();
       }
       if (Fullness < 0)
       {
@@ -97,7 +179,7 @@ namespace Tamagotchis.Models
 
     private void CheckIfDead()
     {
-      if (Energy == 0 || Fullness == 0 || Attention == 0)
+      if (Fullness == 0)
       {
         Energy = 0;
         Fullness = 0;
@@ -110,6 +192,7 @@ namespace Tamagotchis.Models
     public void Feed(int food)
     {
       Fullness += food;
+      Weight += 1;
       if (Fullness >= 100)
       {
         Fullness = 100;
@@ -118,36 +201,25 @@ namespace Tamagotchis.Models
 
     public void Sleep()
     {
-      Energy += 10;
-      if (Energy >= 100)
-      {
-        Energy = 100;
-      }
+      IsSleeping = true;
+      sleepTimer = new Timer();
+      sleepTimer.Interval = 10000;
+      sleepTimer.Elapsed += SleepTimerElapsed;
+      sleepTimer.Start();
     }
-    
+
     public void Play()
     {
-      Attention += 10;
-      Fullness -= 2;
-      Energy -= 2;
-      if (Attention >= 100)
-      {
-        Attention = 100;
-      }
-
-      CheckIfDead();
-    }
-
-    public static Pet Find(int searchId)
-    {
-      if (searchId > 0 && searchId <= _instances.Count)
-      {
-        return _instances[searchId - 1];
-      }
-      else
-      {
-        return null;
-      }
+      playTimer = new Timer();
+      playTimer.Interval = 5000;
+      playTimer.Elapsed += PlayTimerElapsed;
+      playTimer.Start();
     }
   }
 }
+
+// goes to the bathroom & add a clean up option
+// treat for happiness food for fullness
+// 1 year = 1 day
+// amount of times fed = 1 pound each time
+// goes to bed at night and you need to turn the light off to raise attention
