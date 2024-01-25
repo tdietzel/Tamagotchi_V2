dbac1b5d17e102161aca38f8c976b5f1acaeba4f
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-25 15:45:21 -0800

    Add Shop to buy food & toys
    Co-authored-by: Christopher Quattrocchi <chrisquattrocchi@gmail.com>
    Co-authored-by: boboflofo <boboflofo@users.noreply.github.com>

---
+++ Tamagotchi/Models/Shop.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+
+
+namespace Tamagotchis.Models
+{
+  public class Shop
+  {
+    public int Money {get;set;}
+    public Dictionary<string, int> typesOfFood = Food.GetFood();
+    public Dictionary<string, int> typesOfToy = Toy.GetToys();
+  }
+  public class ShopViewModel
+  {
+    public Dictionary<string, int> TypesOfFood {get; set;}
+    public Dictionary<string, int> TypesOfToy {get; set;}
+  }
+}
